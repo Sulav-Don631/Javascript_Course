ae390d0e849e4be58073fef3f73a35c13afa8b1c
Sulav Shrestha <Sulavshrestha631@gmail.com>
2023-09-20 16:08:19 +0545

    fundamentals 2

---
+++ 2nd/assignment2.js
@@ -0,0 +1,54 @@
+"use strict";
+/*
+const calcAverage = (score1, score2, score3) => {
+  const avg = (score1 + score2 + score3) / 3;
+  return avg;
+};
+
+let avgDolphine = calcAverage(44, 23, 71);
+let avgKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphine, avgKoalas) {
+  if (avgDolphine >= 2 * avgKoalas) {
+    console.log(`Dolphine wins (${avgDolphine} vs ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphine) {
+    console.log(`Koala wins (${avgKoalas} vs ${avgDolphine})`);
+  } else {
+    console.log("No one wins");
+  }
+};
+
+checkWinner(avgDolphine, avgKoalas);
+*/
+
+const calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    let tip = (15 / 100) * bill;
+    return tip;
+  } else {
+    let tip = (20 / 100) * bill;
+    return tip;
+  }
+};
+
+const bill = [100, 120, 200, 50, 300, 350, 670];
+console.log(`Bills are ${bill}`);
+const tips = [
+  calcTip(bill[0]),
+  calcTip(bill[1]),
+  calcTip(bill[2]),
+  calcTip(bill[3]),
+  calcTip(bill[4]),
+  calcTip(bill[5]),
+  calcTip(bill[6]),
+];
+
+console.log(`Tips are ${tips}`);
+const totalTips =
+  tips[0] + tips[1] + tips[2] + tips[3] + tips[4] + tips[5] + tips[6];
+console.log(`Total Tips is ${totalTips}`);
+const totalBills =
+  bill[0] + bill[1] + bill[2] + bill[3] + bill[4] + bill[5] + bill[6];
+console.log(`Total Bills is ${totalBills}`);
+const total = totalTips + totalBills;
+console.log(`Total amount will be ${total}`);
---
+++ 2nd/script.js
@@ -0,0 +1,129 @@
+"use strict";
+/*
+let hasDriversLiscence = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriversLiscence = true;
+}
+if (hasDriversLiscence) console.log("I can Drive");
+
+
+function logger() {
+  console.log("My name is Sulav");
+}
+// Calling, Running or Invoking the function
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+
+//Function Declaration
+
+function caclAge1(birthYear) {
+  const age1 = 2037 - birthYear;
+  return age1;
+}
+
+//Function Expression
+
+const calcAge2 = function (birthYear) {
+  const age2 = 2037 - birthYear;
+  return age2;
+};
+
+console.log(caclAge1(1990), calcAge2(1990));
+
+
+//Arrow Function
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1990);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 70 - age;
+  return `${firstName} retires in ${retirement} years`;
+  //return retirement;
+};
+
+const retire = yearsUntilRetirement(1991, "Sulav");
+console.log(retire);
+
+
+function cutFruitPiece(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPiece(apples);
+  const orangePieces = cutFruitPiece(oranges);
+
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+
+//Array
+const friends = ["Sulav", "Ashish", "Prabu"];
+console.log(friends);
+
+const year = new Array(2000, 2001, 2002, 2003);
+console.log(year);
+
+console.log(friends[0]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+const sulav = ["sulav", "Shrestha", 24, friends];
+console.log(sulav);
+
+//Exercise
+const calcAge2 = function (birthYear) {
+  const age2 = 2037 - birthYear;
+  return age2;
+};
+
+const birth = [1990, 1900, 1991, 2000];
+
+const age1 = calcAge2(birth[0]);
+const age2 = calcAge2(birth[1]);
+const age3 = calcAge2(birth[birth.length - 1]);
+
+console.log(age1, age2, age3);
+
+
+const sulav = ["sulav", "Shrestha", 24];
+const newlength = sulav.push("Jay"); // Add elements to the back of the array
+console.log(sulav);
+console.log(newlength);
+
+sulav.unshift("jump"); //Add elements to the front of array
+console.log(sulav);
+
+//Remove elements
+const popped = sulav.pop(); //Removes elements from the back of array
+console.log(popped);
+console.log(sulav);
+sulav.shift(); //Removes elements from the front of the array
+console.log(sulav);
+
+console.log(sulav.indexOf("Shrestha"));
+console.log(sulav.includes("Shrestha"));
+
+if (sulav.includes("Shrestha")) {
+  console.log("Your surname is Shrestha.");
+}
+*/
